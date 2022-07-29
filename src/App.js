1b704ab683938b43426eb41e6306ebfab82da117
Nagateja <valivetiteja2004@gmail.com>
2022-07-29 12:17:22 +0530

    React Router Setup and Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
